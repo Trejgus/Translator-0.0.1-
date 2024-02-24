8fecf8933dbdc39b2ad8c95f39cd9c0562cc23a5
Trejgus <117769309+Trejgus@users.noreply.github.com>
2024-02-24 18:05:36 +0300

    Update settings.py

--- Translator/Translator/settings.py
+++ Translator/Translator/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--0ys*84k=!il7)kkb$to((#^465rk&l84g+s)wfg3srehx39@t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
